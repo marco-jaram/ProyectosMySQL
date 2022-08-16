c7322e1486d53192d6407d0715e954749e4c368e
Marco Jaramillo <marco.jaramillocast@gmail.com>
2022-08-16 16:00:57 -0700

    agregando las consultas a ejercicio u5

--- negocioJoin.sql
+++ negocioJoin.sql
@@ -1,3 +1,12 @@
+-- Ejercicio U5
+-- Crea  tres tablas que permitan administrar la información de una comercializadora. 
+-- Incluye el número de campos que consideres pertinentes. Ingresa 7 registros a cada
+--  tabla  y realiza los siguientes consulta:
+-- Dos consultas empleando el comando join para dos tablas
+-- Dos consultas empleando el comando cross join para dos tablas
+
+
+
 CREATE DATABASE negocioJoin;
 USE negocioJoin;
 
@@ -16,18 +25,20 @@ INSERT INTO cliente VALUES (0, 'Andrea' , 'Gomez' , 'Valdez' , '6648554775');
 SELECT * FROM cliente;
 
 -- Segunda Tabla Pedidos 
-CREATE TABLE IF NOT EXISTS negocioJoin.pedidos (
-    idPedido INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS negocioJoin.ventas (
+    idVenta INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
     idCliente INT,
     descripcion VARCHAR(40),
     fecha DATE,
     FOREIGN KEY (idCliente)
         REFERENCES cliente (idCliente)
 );
-INSERT INTO pedidos VALUES (0, 3 ,'disco duro ssd 250G', '2019-02-05');
-INSERT INTO pedidos VALUES (0, 2 ,'RAM 8Gb', '2017-01-08');
-INSERT INTO pedidos VALUES (0, 1 ,'Monitor hp 21', '2017-05-03');
-INSERT INTO pedidos VALUES (0, 3,'Mouse inalambrico', '2022-08-04');
-INSERT INTO pedidos VALUES (0, 2,'Tecclado mecanico mxcd', '2021-06-08');
+INSERT INTO ventas VALUES (0, 3 ,'disco duro ssd 250G', '2019-02-05');
+INSERT INTO ventas VALUES (0, 2 ,'RAM 8Gb', '2017-01-08');
+INSERT INTO ventas VALUES (0, 1 ,'Monitor hp 21', '2017-05-03');
+INSERT INTO ventas VALUES (0, 3,'Mouse inalambrico', '2022-08-04');
+INSERT INTO ventas VALUES (0, 2,'Tecclado mecanico mxcd', '2021-06-08');
 
-SELECT * FROM pedidos;
+SELECT * FROM ventas;
+
+select * from ventas inner join cliente  on ventas.idCliente ;
